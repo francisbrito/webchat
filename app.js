b2ba5f9bff8d321044f2d6fa6f27c0d7db668400
Francis Brito <fr.br94@gmail.com>
2014-09-22 12:35:45 -0400

    Drop Koa. I think this is a good move since I'm not using anything that requires me to use it.

--- app.js
+++ app.js
@@ -1,18 +1,1 @@
-// Library imports.
-var koa = require('koa');
-var path = require('path');
-var jade = require('jade');
 
-// Application setup.
-var app = koa();
-var views = path.join('.', 'views');
-
-// Helpers
-var view = path.join.bind(path, views);
-
-// Middleware
-app.use(function *() {
-  this.body = jade.renderFile(view('log_in.jade'));
-});
-
-app.listen(3000);
