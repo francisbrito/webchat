b24d52b4e3add9398946f660b6283d58d39c5f71
Francis Brito <fr.br94@gmail.com>
2014-11-04 08:48:45 -0400

    Scaffold chat module.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // index.js -- Application entry point.
 var express = require('express'),
-    app     = express();
+    app     = express(),
+    chat    = require('./lib/webchat-chat');
 
 // Configuration.
 var PORT = process.env.PORT || 3000;
@@ -8,6 +9,8 @@ var PORT = process.env.PORT || 3000;
 app.get('/', function (req, res) {
     res.send('It works!');
 });
+
+app.use('/chat', chat);
 
 app.listen(PORT, function () {
     console.log('Listening at http://localhost:' + PORT);
